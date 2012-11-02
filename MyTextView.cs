baab4ffffa4312b348449fe83cddb7ed44aea92e
Tomasz Cielecki <tomasz@ostebaronen.dk>
2012-11-02 19:09:04 +0100

    Initial Commit.

---
+++ MyTextView.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics;
+using Android.Runtime;
+using Android.Util;
+using Android.Widget;
+
+namespace Cheesebaron.FontSample
+{
+    public class MyTextView : TextView
+    {
+        private const string Tag = "TextView";
+
+        protected MyTextView(IntPtr javaReference, JniHandleOwnership transfer) 
+            : base(javaReference, transfer)
+        {
+        }
+
+        public MyTextView(Context context) 
+            : this(context, null)
+        {
+        }
+
+        public MyTextView(Context context, IAttributeSet attrs) 
+            : this(context, attrs, 0)
+        {
+        }
+
+        public MyTextView(Context context, IAttributeSet attrs, int defStyle) 
+            : base(context, attrs, defStyle)
+        {
+            var a = context.ObtainStyledAttributes(attrs,
+                    Resource.Styleable.CustomFonts);
+            var customFont = a.GetString(Resource.Styleable.CustomFonts_customFont);
+            SetCustomFont(customFont);
+            a.Recycle();
+        }
+
+        public void SetCustomFont(string asset)
+        {
+            Typeface tf;
+            try
+            {
+                tf = Typeface.CreateFromAsset(Context.Assets, asset);
+            }
+            catch (Exception e)
+            {
+                Log.Error(Tag, string.Format("Could not get Typeface: {0} Error: {1}", asset, e));
+                return;
+            }
+
+            if (null == tf) return;
+
+            var tfStyle = TypefaceStyle.Normal;
+            if (null != Typeface) //Takes care of android:textStyle=""
+                tfStyle = Typeface.Style;
+            SetTypeface(tf, tfStyle);
+        }
+    }
+}
---
+++ SampleActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace Cheesebaron.FontSample
+{
+    [Activity(Label = "Font Sample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class SampleActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
